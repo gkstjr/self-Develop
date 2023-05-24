f44673006d48eaec2499a0d4b6750cd829262d54
gkstjr <gkstjr8332@naver.com>
2023-05-24 23:05:12 +0900

    게시판 업데이트 기능 구현 중

--- src/main/java/com/selfdev/board/BoardService.java
+++ src/main/java/com/selfdev/board/BoardService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +30,29 @@ public class BoardService {
 
         Board newBoard = boardRepository.save(board);
         return newBoard;
+    }
+
+    public Board findByBoardId(Long boardId) {
+        Board board = boardRepository.findById(boardId).get();
+
+        return board;
+    }
+
+    public void update(WriteForm writeForm,Long boardId) {
+        Board board = boardRepository.findById(boardId).get();
+                        board.setType(writeForm.getType());
+                        board.setTime(writeForm.getTime());
+                        board.setDaily(writeForm.getDaily());
+                        board.setExecutionAt(writeForm.getExecutionAt());
+                        board.setTitle(writeForm.getTitle());
+                        board.setContent(writeForm.getContent());
+                        board.setUpdateAt(LocalDateTime.now());
+
+        boardRepository.save(board);
+    }
+
+    public void remove(Long boardId) {
+        boardRepository.deleteById(boardId);
+
     }
 }
