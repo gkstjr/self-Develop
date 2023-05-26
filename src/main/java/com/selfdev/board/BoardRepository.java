1142b429f18dd91c35abae0ff64b31f4c166b0b8
gkstjr <gkstjr8332@naver.com>
2023-05-26 18:34:56 +0900

    게시판 조회 기능 구현 중

--- src/main/java/com/selfdev/board/BoardRepository.java
+++ src/main/java/com/selfdev/board/BoardRepository.java
@@ -1,9 +1,14 @@
 package com.selfdev.board;
 
+import com.selfdev.domain.Account;
 import com.selfdev.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
 
 
+
+    Page<Board> findAllByAccount(Account account, PageRequest id);
 }
